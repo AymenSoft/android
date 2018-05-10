55607f444a5610bb288186df2e2d93d372de7541
davigonz <dgonzalez@solidgear.es>
2018-05-10 12:32:38 +0200

    Replace textview with RecyclerView in logs view

--- src/com/owncloud/android/ui/activity/LogHistoryActivity.java
+++ src/com/owncloud/android/ui/activity/LogHistoryActivity.java
@@ -1,7 +1,8 @@
 /**
  *   ownCloud Android client application
  *
- *   Copyright (C) 2016 ownCloud GmbH.
+ *   @author David González Verdugo
+ *   Copyright (C) 2018 ownCloud GmbH.
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License version 2,
@@ -19,14 +20,6 @@
 
 package com.owncloud.android.ui.activity;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.lang.ref.WeakReference;
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
@@ -37,6 +30,8 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.FileProvider;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -45,8 +40,20 @@ import android.widget.TextView;
 
 import com.owncloud.android.R;
 import com.owncloud.android.lib.common.utils.Log_OC;
+import com.owncloud.android.logs.Log;
+import com.owncloud.android.ui.adapter.LogListAdapter;
 import com.owncloud.android.ui.dialog.LoadingDialog;
 import com.owncloud.android.utils.FileStorageUtils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 
 public class LogHistoryActivity extends ToolbarActivity {
@@ -60,25 +67,31 @@ public class LogHistoryActivity extends ToolbarActivity {
     private static final String DIALOG_WAIT_TAG = "DIALOG_WAIT";
 
     private String mLogPath = FileStorageUtils.getLogPath();
-    private File logDIR = null;
+    private File mLogDIR = null;
     private String mLogText;
 
+    private RecyclerView mLogsRecycler;
+
+    private LogListAdapter mLogListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.log_send_file);
+        setContentView(R.layout.logs);
         setupToolbar();
+
+        mLogsRecycler = findViewById(R.id.log_recycler);
+        RecyclerView.LayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        mLogsRecycler.setHasFixedSize(true);
+        mLogsRecycler.setLayoutManager(linearLayoutManager);
 
         setTitle(getText(R.string.actionbar_logger));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        Button deleteHistoryButton = (Button) findViewById(R.id.deleteLogHistoryButton);
-        Button sendHistoryButton = (Button) findViewById(R.id.sendLogHistoryButton);
-        TextView logTV = (TextView) findViewById(R.id.logTV);
+        Button deleteHistoryButton = findViewById(R.id.deleteLogHistoryButton);
+        Button sendHistoryButton = findViewById(R.id.sendLogHistoryButton);
 
         deleteHistoryButton.setOnClickListener(new OnClickListener() {
-            
             @Override
             public void onClick(View v) {
 
@@ -97,20 +110,20 @@ public class LogHistoryActivity extends ToolbarActivity {
 
         if (savedInstanceState == null) {
             if (mLogPath != null) {
-                logDIR = new File(mLogPath);
+                mLogDIR = new File(mLogPath);
             }
 
-            if (logDIR != null && logDIR.isDirectory()) {
+            if (mLogDIR != null && mLogDIR.isDirectory()) {
                 // Show a dialog while log data is being loaded
                 showLoadingDialog();
 
                 // Start a new thread that will load all the log data
-                LoadingLogTask task = new LoadingLogTask(logTV);
+                LoadingLogTask task = new LoadingLogTask();
                 task.execute();
             }
         } else {
             mLogText = savedInstanceState.getString(KEY_LOG_TEXT);
-            logTV.setText(mLogText);
+//            logTV.setText(mLogText);
         }
     }
 
@@ -188,38 +201,37 @@ public class LogHistoryActivity extends ToolbarActivity {
      * Class for loading the log data async
      *
      */
-    private class LoadingLogTask extends AsyncTask<String, Void, String> {
-        private final WeakReference<TextView> textViewReference;
+    private class LoadingLogTask extends AsyncTask<String, Void, ArrayList<Log>> {
+//        private final TextView textViewReference;
 
-        public LoadingLogTask(TextView logTV){
+        public LoadingLogTask(){
             // Use of a WeakReference to ensure the TextView can be garbage collected
-            textViewReference  = new WeakReference<TextView>(logTV);
+//            textViewReference  = logTV;
         }
 
-        protected String doInBackground(String... args) {
+        protected ArrayList<Log> doInBackground(String... args) {
             return readLogFile();
         }
 
-        protected void onPostExecute(String result) {
-            if (textViewReference != null && result != null) {
-                final TextView logTV = textViewReference.get();
-                if (logTV != null) {
-                    mLogText = result;
-                    logTV.setText(mLogText);
-                    dismissLoadingDialog();
-                }
+        protected void onPostExecute(ArrayList<Log> result) {
+            if (result != null) {
+                mLogListAdapter = new LogListAdapter(result);
+                mLogsRecycler.setAdapter(mLogListAdapter);
+                dismissLoadingDialog();
             }
         }
 
         /**
          * Read and show log file info
          */
-        private String readLogFile() {
+        private ArrayList<Log> readLogFile() {
 
             String[] logFileName = Log_OC.getLogFileNames();
 
             //Read text from files
             StringBuilder text = new StringBuilder();
+
+            ArrayList<Log> logList = new ArrayList<>();
 
             BufferedReader br = null;
             try {
@@ -231,10 +243,34 @@ public class LogHistoryActivity extends ToolbarActivity {
                         // Check if FileReader is ready
                         if (new FileReader(file).ready()) {
                             br = new BufferedReader(new FileReader(file));
+                            // Used to split log information (included in several lines) and include it in Log instance
+                            int numberOfLine = 0;
+                            Log log = new Log();
                             while ((line = br.readLine()) != null) {
+                                switch(numberOfLine) {
+                                    case 0:
+                                        numberOfLine++;
+                                        break;
+                                    case 1:
+                                        try {
+                                            SimpleDateFormat simpleDateFormat =
+                                                    new SimpleDateFormat("yyyy/MM/dd H:mm:ss");
+                                            Date logDate = simpleDateFormat.parse(line);
+                                            log.setLogDate(logDate);
+                                        } catch (ParseException e) {
+                                            e.printStackTrace();
+                                        }
+                                        numberOfLine++;
+                                        break;
+                                    case 2:
+                                        log.setLogContent(line);
+                                        logList.add(log);
+                                        numberOfLine = 0;
+                                        break;
+                                }
                                 // Append the log info
-                                text.append(line);
-                                text.append('\n');
+//                                text.append(line);
+//                                text.append('\n');
                             }
                         }
                     }
@@ -253,7 +289,7 @@ public class LogHistoryActivity extends ToolbarActivity {
                 }
             }
 
-            return text.toString();
+            return logList;
         }
    }
 
